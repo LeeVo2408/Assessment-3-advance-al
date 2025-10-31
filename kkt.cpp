78e5e93575069605fd55e754949aa38be5ca300a
Gia Han <giahanphm126@gmail.com>
2025-11-01 08:34:57 +1100

    finish graph contraction  step for kkt boruvka

--- kkt.cpp
+++ kkt.cpp
@@ -2,6 +2,7 @@
 #include "graph.hpp"
 #include <unordered_map>
 #include "kkt.hpp"
+#include <random>
 
 //Boruvka step to return the chosen edges and the connected components (supernode) for next round
 std::pair<std::vector<Graph::Edge>, Graph> boruvkaStep(const Graph& G) {
@@ -9,9 +10,9 @@ std::pair<std::vector<Graph::Edge>, Graph> boruvkaStep(const Graph& G) {
     UnionFind UF(n);
     std::vector<Graph::Edge> cheapest(n);
     std::vector<bool> hasCheapest(n, false);
-    for (int v = 0; v < n; ++v) {
-        for (auto e : *G.neighbours(v)) {
-            if (v != e.v1) continue;                        //avoid duplicate edge
+    for (int u = 0; u < n; ++u) {
+        for (auto e : *G.neighbours(u)) {
+            if (u != e.v1) continue;                        //avoid duplicate edge
             int comp1 = UF.find(e.v1);
             int comp2 = UF.find(e.v2);
 
@@ -29,8 +30,49 @@ std::pair<std::vector<Graph::Edge>, Graph> boruvkaStep(const Graph& G) {
             }
         }
     }
-}
+    std::vector<Graph::Edge> chosen;  //edges chosen in this Boruvka step (will be part of MST)
+    for (int v = 0; v < n; ++v) {
+        if (!hasCheapest[v]) continue;
+        auto e = cheapest[v];
+        int comp1 = UF.find(e.v1);
+        int comp2 = UF.find(e.v2);
 
-Graph kktMST(const Graph& G) {
+        if (comp1 == comp2) continue;
 
+        chosen.push_back(e); //add the chosen edge
+        UF.merge(comp1, comp2);
+    }
+
+    //construct the contract graph G1
+
+    //vertices in the same component in UF are contracted into a supernode
+    std::vector<int> vertexSuperNode(n);                //keep track of vertex's current supernode
+    std::unordered_map<int, int> superNodes;            //map the component to new supernode id
+    int compCount = 0;
+    for (int v = 0; v < n; ++v) {
+        int comp = UF.find(v);
+        //if the component is not mapped yet
+        if (superNodes.find(comp) == superNodes.end()) {
+            superNodes[comp] = compCount;
+            vertexSuperNode[v] = superNodes[comp];
+            ++compCount;
+        }
+        else {
+            vertexSuperNode[v] = superNodes[comp];
+        }
+    }
+    //now add edges to the contracted graph
+    Graph G1(compCount);
+    for (int u = 0; u < n; ++u) {
+        for (auto e : *G.neighbours(u)) {
+            if (u != e.v1) continue;                    //avoid duplicate edge
+            int su = vertexSuperNode[u];                //supernode of vertex u
+            int sv = vertexSuperNode[e.v2];             //supernode of vertex v
+
+            if (su == sv) continue;                     //both endpoints are in same supernode (delete self-loop)
+            
+            G1.addEdge({e.weight, su, sv});
+        }
+    }
+    return {chosen, G1};
 }
